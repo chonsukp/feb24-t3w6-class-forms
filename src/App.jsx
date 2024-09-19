89ccd4affa90662f796fcb70c4ba371083a5b388
chonsukp <chonsukp@gmail.com>
2024-09-19 20:42:53 +1000

    feat: toggle edit mode and example posts

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import SocialMediaPostContainer from './components/SocialMediaPostContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <SocialMediaPostContainer />
+      <SocialMediaPostContainer />
+      <SocialMediaPostContainer />
+      <SocialMediaPostContainer />
+      <SocialMediaPostContainer />
+      <SocialMediaPostContainer />
+      <SocialMediaPostContainer />
+      <SocialMediaPostContainer />
+      <SocialMediaPostContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SocialMediaPostContainer.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import ReadonlyPost from "./ReadonlyPost";
+import EditablePost from "./EditablePost";
+
+export default class SocialMediaPostContainer extends React.Component {
+	constructor(){
+		super();
+
+		this.state = {
+			isEditing: false,
+			author: "",
+			dateCreated: Date.now(),
+			lastUpdated: Date.now(),
+			location: "",
+			content: ""
+		}
+	}
+
+	updatePostData = (newAuthor, newLocation, newContent) => {
+		this.setState({
+			author: newAuthor,
+			lastUpdated: Date.now(),
+			location: newLocation,
+			content: newContent
+		});
+	}
+
+	toggleEditMode = () => {
+		this.setState({isEditing: !this.state.isEditing});
+	}
+
+	render(){
+
+		// this.props.author 
+		// this.props.parentState.author 
+		
+
+		if (this.state.isEditing){
+			return(
+				<>
+					<button onClick={this.toggleEditMode}>
+						Toggle Edit Mode
+					</button>
+					<EditablePost
+						author={this.state.author} 
+						dateCreated={this.state.dateCreated} 
+						lastUpdated={this.state.lastUpdated} 
+						location={this.state.location}
+						content={this.state.content}
+						updateData={this.updatePostData}
+
+						parentState={this.state}
+					/>
+				</>
+				
+			);
+		} else {
+			return(
+				<>
+					<button onClick={this.toggleEditMode}>
+						Toggle Edit Mode
+					</button>
+					<ReadonlyPost 
+						author={this.state.author} 
+						dateCreated={this.state.dateCreated} 
+						lastUpdated={this.state.lastUpdated} 
+						location={this.state.location}
+						content={this.state.content}
+
+						parentState={this.state}
+					/>
+				</>
+				
+			);
+		}
+	}
+}
